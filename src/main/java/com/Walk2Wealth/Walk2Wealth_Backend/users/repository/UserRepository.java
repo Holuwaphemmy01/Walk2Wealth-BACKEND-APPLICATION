3328f0c8b12daba5c25202495620f3254e0c391f
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2025-02-27 18:07:54 +0100

    UserPrincipal service implementation in the spring security

--- src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/users/repository/UserRepository.java
+++ src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/users/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.Walk2Wealth.Walk2Wealth_Backend.users.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserName(String username);
 }
--- src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/users/service/userDetailService/DetailService.java
+++ src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/users/service/userDetailService/DetailService.java
@@ -1,4 +1,28 @@
 package com.Walk2Wealth.Walk2Wealth_Backend.users.service.userDetailService;
 
-public class DetailService {
+import com.Walk2Wealth.Walk2Wealth_Backend.users.model.User;
+import com.Walk2Wealth.Walk2Wealth_Backend.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipal;
+
+@Service
+public class DetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserPrincipal(user);
+    }
 }
