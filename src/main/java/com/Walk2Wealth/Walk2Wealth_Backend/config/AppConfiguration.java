65a28d322bd9e221dbcecdddee41c155705d9c58
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2025-02-28 17:01:08 +0100

    Implementation of the login component

--- src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/config/AppConfiguration.java
+++ src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/config/AppConfiguration.java
@@ -3,9 +3,11 @@ package com.Walk2Wealth.Walk2Wealth_Backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -47,11 +49,18 @@ public class AppConfiguration {
         return authProvider;
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+    }
+
 
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 
 }
---
+++ src/main/java/com/Walk2Wealth/Walk2Wealth_Backend/users/service/login/UserLoginService.java
@@ -0,0 +1,4 @@
+package com.Walk2Wealth.Walk2Wealth_Backend.users.service.login;
+
+public interface UserLoginService {
+}
